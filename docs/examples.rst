3643b41a6e6dc5dfe685a10c7bf9e6a34afbbc34
sabas1080 <sabasjimenez@gmail.com>
2021-03-29 18:50:39 -0600

    init circuitpython library :D

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/pn7150_simpletest.py
+    :caption: examples/pn7150_simpletest.py
+    :linenos:
---
+++ examples/pn7150_simpletest.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Electronic Cats for Electronic Cats
+#
+# SPDX-License-Identifier: Unlicense
